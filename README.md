f582fe5ac88474f38f2c97aa3da2318831c45311
Justin Howe <howejust@gmail.com>
2019-12-18 10:23:39 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello-World-Hack-2020
+# Hello-World-Hack-2020
---
+++ script.js
@@ -0,0 +1,27 @@
+/* Code for the following slideshow... */
+
+//Creates the variable for the slideIndex number and then shows the 1st slide by default
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  //If the slideNumber is already the last one, put the slideNumber to be the first one
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
